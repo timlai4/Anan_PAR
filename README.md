0c6268cea29fc82b9ca609f9185a0c0853dba313
Tim Lai <tlai4@disroot.org>
2020-10-17 22:04:41 -0400

    Alignment

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Anan_PAR-
+# Anan_PAR
 
 ## Data Retrieval
 The reference genome annotation and assembly were downloaded from Ensembl.
@@ -11,3 +11,5 @@ Two samples, one from each sex, were obtained and QC'd
 ```{bash}
 singularity exec -eCB `pwd` -H `pwd` zent_tools_software_v0.4.sif Rscript fetch_data.R
 ```
+The alignment is performed with [bwa-mem2](https://github.com/bwa-mem2/bwa-mem2). 
+We can run this in our do_align.sh script. 
---
+++ do_align.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+set -u
+set -o pipefail
+
+curl -L https://github.com/bwa-mem2/bwa-mem2/releases/download/v2.0pre2/bwa-mem2-2.0pre2_x64-linux.tar.bz2 \
+  | tar jxf -
+bwa-mem2-2.0pre2_x64-linux/bwa-mem2 index GCF_000952055.2_Anan_2.0_genomic.fna.gz
+
+bwa-mem2-2.0pre2_x64-linux/bwa-mem2 mem -t 4 GCF_000952055.2_Anan_2.0_genomic.fna.gz SRR7608134/* > SRR7608134.sam &
+bwa-mem2-2.0pre2_x64-linux/bwa-mem2 mem -t 4 GCF_000952055.2_Anan_2.0_genomic.fna.gz SRR7608135/* > SRR7608135.sam
